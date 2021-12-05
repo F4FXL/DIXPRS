a066783d56506463ce6ef2a19f0d2884b073cd21
Geoffrey Merck <F4FXL@users.noreply.github.com>
2021-12-05 14:42:18 +0100

    add libdixfilter.py

---
+++ dixlibfilter.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+
+####################################################
+# APRS digipeater and gateway for amateur radio use
+#
+# (C) HA5DI - 2012-2021
+# (C) F4FXL - 2021
+#
+# http://sites.google.com/site/dixprs/
+####################################################
+
